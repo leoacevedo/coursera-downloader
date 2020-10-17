46759810fee3585246f2ea98505e8a8d5e8e90c1
Leonardo Acevedo <leo.acevedo@gmail.com>
2020-10-17 01:06:35 -0300

    Improvements: - works more reliably (more than yesterday anyway) - uses userscript privileged AJAX API to avoid CORS errors - works both in Greasemonkey and Tampermonkey - prompts the user to download everything as soon as it knows what "everything" is. If they refuse, they can do it later by pressing the orange button - added comments to document the process and some decisions

--- coursera-downloader.user.js
+++ coursera-downloader.user.js
@@ -1,65 +1,94 @@
 // ==UserScript==
 // @name        Coursera Downloader
+// @author      Leonardo Acevedo (leo.acevedo@gmail.com)
 // @namespace   https://leoacevedo.com/userscripts/coursera-downloader
 // @description Download Coursera resources(mp4, pdf, ppt, txt, srt...) from a lecture page. 'Download all materials' should appear at the top of the page
 // @match       https://www.coursera.org/*/lecture/*
 // @match       https://www.coursera.org/*/lecture/index
 // @match       https://www.coursera.org/*/lecture
+// @grant       GM_xmlhttpRequest
+// @grant       GM.xmlHttpRequest
 // @version     1.0
 // ==/UserScript==
 
 window.addEventListener('load', () => {
+    // Work both with GreaseMonkey and TamperMonkey
+    const xmlHttpRequest = window.GM_xmlhttpRequest || GM.xmlHttpRequest
+    
+    // Information about all the downloads from this page
     const downloadResources = []
-    var downloadBtn = null;
 
+    // Button to trigger all the downloads. It is enabled only when downloadResources contains something
+    const downloadButton = document.createElement("button")
+    downloadButton.disabled = true
+    downloadButton.class = "downloadBtn"
+    downloadButton.style = "color: white; background: orange; font-weight=700"
+    downloadButton.innerHTML = "<span>Download all materials</span>"
+    downloadButton.onclick = () => { downloadAll() }
+ 
     function injectButton() {
         var videoName = document.querySelector('h4')
         if (!videoName) {
-            return false;
+            return false
         }
 
-        const downloadButtonHTML = "&nbsp; \
-            <button class='downloadBtn' style='color: white; background: orange; font-weight=700'>\
-                <span>Download all materials</span>\
-            </button>";
-        videoName.insertAdjacentHTML("beforeEnd", downloadButtonHTML);
-        downloadBtn = document.getElementsByClassName("downloadBtn")[0]
-        downloadBtn.onclick = () => { downloadAll() }
-        return true;
+        videoName.insertAdjacentHTML("beforeEnd", "&nbsp;")
+        videoName.insertAdjacentElement("beforeEnd", downloadButton)
+        return true
     }
 
-    function extractResources() {
+    function getDownloadResources() {
+        // Breadcrumbs and the dropdown content have text that helps in giving the downloads proper names
         const breadcrumbs = document.querySelectorAll('.breadcrumb-title')
         const groupName = breadcrumbs[1].querySelector("span").innerHTML
-        const downloadSection = document.querySelector('.rc-DownloadsDropdown.bt3-dropdown')
-        const downloadButton = downloadSection.getElementsByTagName('button')[0]
-        downloadButton.click()
+        const downloadDropdownContainer = document.querySelector('.rc-DownloadsDropdown.bt3-dropdown')
+        
+        // Open the dropdown. Unfortunately I don't know how else to get the download information
+        const downloadDropdownButton = downloadDropdownContainer.getElementsByTagName('button')[0]
+        downloadDropdownButton.click()
 
         setTimeout(() => {
-            const items = downloadSection.querySelectorAll('a.menuitem')
+            const items = downloadDropdownContainer.querySelectorAll('a.menuitem')
             for(var i = 0, I = items.length; i < I; i++) {
-                var anchor = items[i];
+                var anchor = items[i]
                 var resourceUrl = anchor.href.trim()
                 var fileExtension = guessResourceExtension(resourceUrl)
                 var videoName = document.querySelector('h4').childNodes[0].textContent
-                var lessonName = formatLessonName(videoName);
+                var lessonName = formatLessonName(videoName)
                 
-                var anchorTexts = anchor.querySelectorAll('span');
-                var resourceName = anchorTexts[0].innerText;
+                var anchorTexts = anchor.querySelectorAll('span')
+                var resourceName = anchorTexts[0].innerText
 
+                // Give the video and the subtitle the same name so video players pick the subtitle automatically
                 if(fileExtension == 'mp4' || fileExtension == 'vtt') {
                         resourceName = "Video"
                 }
+
+                // Remove the file extension from file names that already have it
                 if (resourceName.endsWith('.' + fileExtension)) {
                     resourceName = resourceName.substr(0, resourceName.length - 1 - fileExtension.length)
                 }
-                var fileName = formatFileName(groupName + " - " + lessonName + ' - ' + resourceName + "." + fileExtension);
-                anchor.download = fileName;
+                var fileName = formatFileName(groupName + " - " + lessonName + ' - ' + resourceName + "." + fileExtension)
                 downloadResources.push({ url: resourceUrl, fileName: fileName })
-                console.log(fileName)
             }
-            downloadButton.click()
-        }, 2000)
+
+            // Close the dropdown
+            downloadDropdownButton.click()
+
+            var resourceCount = downloadResources.length
+            var canDownload = resourceCount > 0
+            if (canDownload) {
+                downloadButton.disabled = false
+
+                var shouldDownload = confirm("Download " + resourceCount + " files?")
+                if (shouldDownload) {
+                    downloadAll()
+                }
+            } else {
+                // Very likely the connection is too slow. Retry until stuff is ready
+                setTimeout(() => { getDownloadResources() }, 10_000)
+            }
+        }, 0)
     }
 
     function downloadAll() {
@@ -67,50 +96,58 @@ window.addEventListener('load', () => {
     }
 
     function downloadResourcesFrom(index) {
-        const resource = downloadResources[index];
-        fetch(resource.url)
-            .then((result) => {
-                return result.arrayBuffer()
-            })
-            .then((buffer) => {
-                const blob = new Blob([buffer], { type: 'application/octet-stream' })
-                const link = document.createElement('a');
-                link.href = URL.createObjectURL(blob);
-                link.innerText = 'Open the array URL';
-                link.download = resource.fileName
-                link.click()
-            })
-            .then((ignore) => {
-                // Next resource
-                if (index + 1 < downloadResources.length) {
-                    downloadResourcesFrom(index + 1)
+        const resource = downloadResources[index]
+        const goToNext = () => {
+            if (index + 1 < downloadResources.length) {
+                downloadResourcesFrom(index + 1)
+            }
+        }
+
+        xmlHttpRequest({
+            method: "GET",
+            url: resource.url,
+            responseType: "blob",
+            overrideMimeType: "application/octet-stream", // force browser to download stuff rather than opening a tab
+            onerror: goToNext,
+            onload: (result) => {
+                try {
+                    const blob = result.response
+                    const link = document.createElement('a')
+                    link.href = URL.createObjectURL(blob)
+                    link.innerText = 'Open the array URL'
+                    link.download = resource.fileName
+                    link.click()
+                } finally {
+                    goToNext()
                 }
-            })
+            }
+        })
     }
 
     function formatLessonName(str) {
-        return str.replace(/\(\d+:\d*:?\d*\)\s*$/, "").trim();
+        return str.replace(/\(\d+:\d*:?\d*\)\s*$/, "").trim()
     }
 
     function formatFileName(str) {
-        var replaceChar = '';
+        var replaceChar = ''
         return str
             .replace(/[,/\:*?""<>|]/g, replaceChar) // forbidden characters
-            .replace(/^\./, replaceChar); // cannot start with dot (.)
+            .replace(/^\./, replaceChar) // cannot start with dot (.)
     }
 
     function guessResourceExtension(url) {
-        var urlParts = url.split('?');
-        var ext = /fileExtension=(\w{1,5})$/g.exec(urlParts[1]) || /\.(\w{1,5})$/g.exec(urlParts[0]);
-        var result = ext ? ext[1] : "";
-        return result;
+        var urlParts = url.split('?')
+        var ext = /fileExtension=(\w{1,5})$/g.exec(urlParts[1]) || /\.(\w{1,5})$/g.exec(urlParts[0])
+        var result = ext ? ext[1] : ""
+        return result
     }
 
     function doTheJob() {
+        // AJAX-based UI takes some time to generate. Wait for it
         if (injectButton()) {
-            extractResources()
+            setTimeout(() => { getDownloadResources() }, 2000)
         } else {
-            setTimeout(arguments.callee, 2000);            
+            setTimeout(doTheJob, 1000)
         }
     }
 
